eb4e0ea182db08e0c82dcb6ad0114405faf6baf3
jameskiongo <kiongojames2000@gmail.com>
2025-04-24 13:44:40 +0300

    initialized pytest and added tests

--- backend/client/tests.py
+++ backend/client/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
-
 # Create your tests here.
+import pytest
+from django.urls import reverse
+
+from client.models import Client
+from client.serializers import ClientSerializer
+
+
+@pytest.mark.django_db
+def test_get_clients(client):
+    url = reverse("clients")
+    response = client.get(url)
+
+    articles = Client.objects.all()
+    expected_data = ClientSerializer(articles, many=True).data
+
+    assert response.status_code == 200
+    assert response.data == expected_data
