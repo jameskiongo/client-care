fe365fce1975e9730ddad401665a6bd6eff449df
jameskiongo <kiongojames2000@gmail.com>
2025-04-25 11:35:22 +0300

    adding remainder for search functionality

--- backend/client/views.py
+++ backend/client/views.py
@@ -24,3 +24,6 @@ class ClientView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# TODO: Filter clients by username
